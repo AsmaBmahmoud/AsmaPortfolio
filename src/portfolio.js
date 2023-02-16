c79fd54dbd869d2a38eb28f3baa2e323623d9dd9
AsmaBmahmoud <51321695+AsmaBmahmoud@users.noreply.github.com>
2023-02-16 17:06:57 +0100

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -307,8 +307,8 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+  number: "+33",
+  email_address: "benmahmoudasm57@gmail.com"
 };
 
 
